c3b8990e7ab62e057697be072524cbd73abf42c2
mnubhan <76425959+mnubhan@users.noreply.github.com>
2023-04-30 23:47:57 +0100

    Update answer.js

--- answer.js
+++ answer.js
@@ -13,3 +13,27 @@ function titleCase(title, minorWords) {
   })}
   return newArr ? newArr.join(" "):""
 }
+
+function titleCase(title, minorWords) {
+  var minorWords = typeof minorWords !== "undefined" ? minorWords.toLowerCase().split(' ') : [];
+  return title.toLowerCase().split(' ').map(function(v, i) {
+    if(v != "" && ( (minorWords.indexOf(v) === -1) || i == 0)) {
+      v = v.split('');
+      v[0] = v[0].toUpperCase();
+      v = v.join('');
+    }
+    return v;
+  }).join(' ');
+}
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+}
+
+function titleCase(title, minorWords) {  
+  var titleAr = title.toLowerCase().split(' '),
+      minorWordsAr = minorWords ? minorWords.toLowerCase().split(' ') : [];
+    
+  return titleAr.map(function(e, i){return minorWordsAr.indexOf(e) === -1 || i === 0 ? e.capitalize() : e })
+                .join(' ');
+}
